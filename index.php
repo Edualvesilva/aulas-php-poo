5cfdf6165f2d3cad6735e09eb55f9317de53ef54
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-09-21 11:57:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 1</title>
+    <title>Exemplo 2</title>
 </head>
 <body>
     
-<h1>PHP com PDO - Exemplo 1</h1>
+<h1>PHP com PDO - Exemplo 2</h1>
 <hr>
 
 <h2>Assuntos abordados:</h2>
 <ul>
-    <li>Criação de Classes</li>
-    <li>Importação do arquivo de classes</li>
-    <li>Criação de instâncias/objetos</li>
+    <li>Acesso direto ás propriedades</li>
+    <li>Atribuição e leitura de dados</li>
+    <li>Chamada de método</li>
 </ul>
 <?php
 // Importando a Classe
@@ -23,7 +23,20 @@ require_once "src/Cliente.php";
 // Criando instâncias da Classe (objetos!)
 $clienteA = new Cliente;
 $clienteB = new Cliente;
+
+// Acesso e Atribuição
+$clienteA->nome = "Eduardo";
+$clienteB->nome = "Keanus Reeves";
+
+$clienteA->telefones = ["11-2134-0300","11-95655-5555"];
+$clienteB->telefones = ["777-777-777"];
+
+$clienteA->senha =password_hash("123abc",PASSWORD_DEFAULT);
 ?>
-<pre><?=var_dump($clienteA)?></pre>
+<hr>
+<h3><?=$clienteA->nome?></h3>
+<h3><?=$clienteB->nome?></h3>
+<pre><?=var_dump($clienteA,$clienteB)?></pre>
+<h2>Dados dos Objetos</h2>
 </body>
 </html>
