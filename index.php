0ac4dc14e0a559b78e1c38c611f24854aa571ff4
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-09-29 08:47:25 -0300

    Erro/as classes tem o mesmo nome

--- index.php
+++ index.php
@@ -4,47 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 8</title>
+    <title>Exemplo 9</title>
 </head>
 
 <body>
 
-    <h1>PHP com POO - Exemplo 8</h1>
+    <h1>PHP com POO - Exemplo 9</h1>
     <hr>
 
     <h2>Assuntos abordados:</h2>
 
     <ul>
-    <li>Propriedades e métodos estáticos</li>
-    <li>Acesso direto sem necessidade de objetos/instâncias</li>
-    <li>Uso do <code>self</code> para acesso (dentro de classe) aos recursos estáticos</li>
+    <li>Namespaces: agrupamento de recursos (classes,funções,constantes)</li>
+    <li>Prevenção de conflitos entre classes de mesmo nome</li>
+    <li>Configurar e usar namespaces e alias</li>
     </ul>
-    
+  
 <?php
-require_once "src/PessoaFisica.php";
-$cliente1 = new PessoaFisica;
-$cliente1->setNome("Astrogildo");
-$cliente1->setIdade(78);
+/* Só de fazer o require/importação das classes, 
+já dá erro no servidor devido a terem o mesmo nome*/
+require_once "src/fornecedores/Pagamento.php";
+require_once "src/prestadores/Pagamento.php"; 
 
-$cliente2 = new PessoaFisica;
-$cliente2->setNome("Enzo");
-$cliente2->setIdade(19);
-
-require_once "src/Utilitarios.php";
-Utilitarios::obterData();
 ?>
-<h2>Atendimentos do dia: <?=Utilitarios::$dataAtual?></h2>
-
-<h3>Cliente <?=$cliente1->getNome()?></h3>
-<p>Tipo de Atendimento <?=Utilitarios::definirAtendimento($cliente1->getIdade())?></p>
-
-<h3>Cliente <?=$cliente2->getNome()?></h3>
-<p>Tipo de Atendimento <?=Utilitarios::definirAtendimento($cliente2->getIdade())?></p>
-
-
-
-
-
 </body>
 
 </html>
